e2bcff5108e362c59c0bef6ae641c16aeb5c1e95
ww8007 <wshmin@naver.com>
2022-04-07 00:13:44 +0900

    feat: add root render

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('main');
+const root = createRoot(container);
+import App from './App';
+root.render(<App />);
