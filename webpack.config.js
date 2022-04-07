217737bfb3fc925e01a94afac1a1f20daba17cd4
ww8007 <wshmin@naver.com>
2022-04-07 19:39:40 +0900

    rename: Move index.tsx to root dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,43 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const TerserJsPlugin = require('terser-webpack-plugin');
 module.exports = {
-	entry: './src/index.tsx',
-	output: {
-		path: path.join(__dirname, 'build'),
-		filename: 'build.js'
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-		modules: [path.join(__dirname, 'src'), 'node_modules']
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx|js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						'@babel/preset-env',
-						'@babel/preset-typescript',
-						[
-							'@babel/preset-react',
-							{ runtime: 'automatic', importSource: '@emotion/react' }
-						]
-					],
-					plugins: [['@babel/plugin-transform-runtime'], ['@emotion']]
-				}
-			}
-		]
-	},
-	plugins: [
-		new htmlWebpackPlugin({
-			template: './build/index.html',
-			filename: 'index.html'
-		})
-	],
-	optimization: {
-		minimizer: [
-			new TerserJsPlugin({
-				extractComments: false
-			})
-		]
-	}
+  entry: './index.tsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-typescript',
+            ['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }],
+          ],
+          plugins: [['@babel/plugin-transform-runtime'], ['@emotion']],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './build/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserJsPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
 };
