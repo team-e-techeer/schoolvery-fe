5a5dbb2de45d4539fd9fedb27decff08c63f2eb8
ww8007 <wshmin@naver.com>
2022-04-07 19:53:30 +0900

    feat: Add recoil, react-query setting

--- index.tsx
+++ index.tsx
@@ -1,5 +1,17 @@
 import { createRoot, Root } from 'react-dom/client';
+import App from './src/App';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 const container = document.getElementById('main');
 const root: Root | null = container && createRoot(container);
-import App from './src/App';
-root && root.render(<App />);
+const queryClient = new QueryClient();
+
+root &&
+  root.render(
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
