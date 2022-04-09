c049d81a0fbdcf8dd8d102dfc6d945b00f8a20a5
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-09 17:43:33 +0900

    feat: Add router paths main, login, myInfo, chat, writing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from 'pages/MainPage';
+import LoginPage from 'pages/LoginPage';
+import PersonalPage from 'pages/PersonalPage';
+import ChattingPage from 'pages/ChattingPage';
+import WritingPage from 'pages/WritingPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/myInfo" element={<PersonalPage />} />
+        <Route path="/chat" element={<ChattingPage />} />
+        <Route path="/writing" element={<WritingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
