1c743e8e9a6e442fd2cce939c033f46ac683e04c
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-11 03:21:02 +0900

    feat: Add webpack setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const toPath = _path => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -9,5 +12,26 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }],
+          '@babel/preset-typescript',
+          '@babel/preset-env',
+        ],
+      },
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    };
+    return config;
   },
 };
