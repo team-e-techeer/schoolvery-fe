d92087c83a829d98d999c5c86fffc2e0b0699c84
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-11 03:22:35 +0900

    feat: Add Header component props 1. title 2. Left : ReactNode 3. Right : ReactNode 4. secondRight: ReactNode

---
+++ src/components/Header.tsx
@@ -0,0 +1,69 @@
+import { css } from '@emotion/react';
+import type { ReactNode } from 'react';
+export interface Props {
+  title?: string;
+  Left?: () => ReactNode;
+  Right?: () => ReactNode;
+  secondRight?: () => ReactNode;
+}
+
+export default function Header({ title, Left, Right, secondRight }: Props) {
+  return (
+    <>
+      <header
+        css={css`
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          height: 5rem;
+          background-color: #fc775a;
+        `}
+      >
+        <div
+          css={css`
+            flex: 1;
+          `}
+        >
+          {Left && Left()}
+        </div>
+        <div
+          css={css`
+            flex: 1;
+            text-align: center;
+          `}
+        >
+          <h1
+            css={css`
+              color: #fff;
+            `}
+          >
+            {title}
+          </h1>
+        </div>
+        <div
+          css={css`
+            display: flex;
+            flex: 1;
+            flex-direction: row;
+            justify-content: flex-end;
+          `}
+        >
+          <div
+            css={css`
+              flex: 0.3;
+              background-color: 'transparent';
+            `}
+          />
+          {secondRight && secondRight()}
+          <div
+            css={css`
+              flex: 0.3;
+              background-color: 'transparent';
+            `}
+          />
+          {Right && Right()}
+        </div>
+      </header>
+    </>
+  );
+}
