d45fc53c4bb8f52271d650845daaa91ac46c734b
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-02 22:48:13 +0900

    style: Add InputBlock, LinkText with form

--- src/pages/Login/LoginPage/LoginPage.style.ts
+++ src/pages/Login/LoginPage/LoginPage.style.ts
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
+import colors from '../../../constants/colors';
+import { Link } from 'react-router-dom';
 
 export const IconWrapper = styled.div`
   flex: 1;
-  margin-left: 3rem;
+  margin-left: 2rem;
   .button {
     border: none;
     cursor: pointer;
@@ -19,15 +21,34 @@ export const Title = styled.h2`
   text-align: center;
 `;
 
+export const InputBlock = styled.form`
+  width: 80%;
+  margin: auto;
+`;
+
 export const Input = styled.input`
-  display: block;
-  background-color: #eeeeee;
+  display: flex;
+  background-color: #e7f0fe;
   margin: 3rem 0;
   border-radius: 0.7rem;
-  width: 80%;
-  margin-left: 10%;
+  width: 100%;
   padding: 1.5rem;
+  font-size: 1.5rem;
   ::placeholder {
     opacity: 0.6;
+    font-size: 1.5rem;
   }
+  :focus {
+    background-color: ${colors.blue100};
+  }
+`;
+
+export const LinkText = styled(Link)`
+  display: flex;
+  flex-direction: row-reverse;
+  text-decoration: none;
+  margin-top: 1.5rem;
+  margin-right: 10%;
+  color: #646363;
+  font-size: 1.2rem;
 `;
