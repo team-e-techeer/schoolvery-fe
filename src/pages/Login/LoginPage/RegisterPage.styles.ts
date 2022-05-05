0ece309b193d1e89d455a388a1e50c27af5d3645
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-05 22:56:53 +0900

    rename: Fix style to styles with pascalCase

--- src/pages/Login/LoginPage/RegisterPage.styles.ts
+++ src/pages/Login/LoginPage/RegisterPage.styles.ts
@@ -22,11 +22,16 @@ export const JoinBlock = styled.form`
   margin: auto;
 `;
 
+export const InputField = styled.div`
+  display: block;
+  box-sizing: border-box;
+  margin-top: 1.5rem;
+`;
+
 export const InputOverText = styled.span`
   display: block;
   margin-left: 0.1rem;
   font-size: 1rem;
-  margin-top: 1.5rem;
   font-size: 1.3rem;
   font-weight: 500;
 `;
@@ -34,7 +39,7 @@ export const InputOverText = styled.span`
 export const Input = styled.input`
   display: flex;
   background-color: #e7f0fe;
-  margin: 2rem 0;
+  margin-top: 1rem;
   border-radius: 0.7rem;
   width: 100%;
   padding: 1.5rem;
@@ -56,4 +61,10 @@ export const LinkText = styled(Link)`
   margin-right: 10%;
   color: #646363;
   font-size: 1.2rem;
+`;
+
+export const AlertText = styled.span`
+  display: none;
+  color: ${colors.red500};
+  font-size: 1.3rem;
 `;
