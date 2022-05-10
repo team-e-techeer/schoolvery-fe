5267266420b804d82664abcac542a8765dde54c3
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-10 19:33:34 +0900

    chore: Add setting for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
