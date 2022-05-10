7430f08b59c4c899fdbe3086e86168884f05cfb3
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-11 05:16:23 +0900

    rename: Rename PersonalPage to MyInfoPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/Login/LoginPage/LoginPage';
 import RegisterPage from './pages/Login/LoginPage/RegisterPage';
-import PersonalPage from './pages/PersonalPage';
+import MyInfoPage from './pages/MyInfoPage';
 import ChattingPage from './pages/ChattingPage';
 import WritingPage from './pages/WritingPage';
 
@@ -10,10 +10,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/myInfo" element={<PersonalPage />} />
+        <Route path="/myInfo" element={<MyInfoPage />} />
         <Route path="/chat" element={<ChattingPage />} />
         <Route path="/writing" element={<WritingPage />} />
       </Routes>
