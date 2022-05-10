a35597f8b5967f00114a23f82ed016dbd0cc5ce8
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-11 00:44:03 +0900

    move: Rename PersonalPage to MyInfoPage

---
+++ src/pages/MyInfoPage.tsx
@@ -0,0 +1,13 @@
+import BottomNavigation from '@/components/BottomNavigation';
+import Header from '@/components/Header';
+
+function PersonalPage() {
+  return (
+    <>
+      <Header />
+      <BottomNavigation />
+    </>
+  );
+}
+
+export default PersonalPage;
