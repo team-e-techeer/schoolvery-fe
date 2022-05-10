a47483b5755b857d4d8770406a4a10d33b679a75
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-10 20:22:34 +0900

    style: Add AlertText span style

--- src/pages/Login/LoginPage/LoginPage.styles.ts
+++ src/pages/Login/LoginPage/LoginPage.styles.ts
@@ -29,7 +29,8 @@ export const InputBlock = styled.form`
 export const Input = styled.input`
   display: flex;
   background-color: #e7f0fe;
-  margin: 3rem 0;
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
   border-radius: 0.7rem;
   width: 100%;
   padding: 1.5rem;
@@ -51,4 +52,11 @@ export const LinkText = styled(Link)`
   margin-right: 10%;
   color: #646363;
   font-size: 1.2rem;
+`;
+
+export const AlertText = styled.span`
+  display: none;
+  color: ${colors.red500};
+  font-size: 1.3rem;
+  margin-bottom: 1.5rem;
 `;
