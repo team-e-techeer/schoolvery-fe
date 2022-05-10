66ed668ca9680fdaccf25de7eb401480bfa32cf4
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-10 19:32:51 +0900

    chore: Add setting for jest test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
+  preset: 'ts-jest',
+  modulePaths: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(png|pdf|svg|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|style|less|sass|scss|svg)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
   transformIgnorePatterns: ['<rootDir>/node_modules/', 'dist', 'build'],
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
   snapshotSerializers: ['@emotion/jest/serializer' /* if needed other snapshotSerializers should go here */],
 };
