5f91307f392384a3ab9a865b473cc8e46b0e3579
장동현 <54137044+ww8007@users.noreply.github.com>
2022-05-12 00:15:26 +0900

    feat: Add test render JoinSummary

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import BottomNavigation from '@/components/BottomNavigation';
+import JoinSummary from '@/components/JoinSummary';
 import Header from '../components/Header';
 
 function MainPage() {
   return (
     <>
       <Header />
+      <>
+        <JoinSummary title="hi" time={{ left: '1', post: '1' }} people={{ current: 1, total: 3 }} />
+      </>
       <BottomNavigation />
     </>
   );
