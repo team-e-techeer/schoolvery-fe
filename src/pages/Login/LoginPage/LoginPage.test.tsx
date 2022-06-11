5819d5ae2775de89a5e673fcf1b32ca12ccbbb9a
장동현 <54137044+ww8007@users.noreply.github.com>
2022-06-11 19:59:28 +0900

    fix: Remove not used thing

--- src/pages/Login/LoginPage/LoginPage.test.tsx
+++ src/pages/Login/LoginPage/LoginPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import LoginPage from './LoginPage';
