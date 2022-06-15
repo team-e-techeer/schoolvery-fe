83f0386ac0e328d661ef57d6b1d2f205f60b8b65
장동현 <54137044+ww8007@users.noreply.github.com>
2022-06-15 21:22:10 +0900

    feat: Add render ChatPage, ChatRoomPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import MainPage from './pages/Main/MainPage';
 import LoginPage from './pages/Login/LoginPage/LoginPage';
 import RegisterPage from './pages/Login/LoginPage/RegisterPage';
 import MyInfoPage from './pages/MyInfoPage';
-import ChattingPage from './pages/ChattingPage';
 import WritingPage from './pages/Writing/WritingPage';
 import SearchPage from './pages/Main/SearchPage';
 import SearchDetailPage from './pages/Main/SearchDetailPage';
-//import SearchWithCategory from './pages/Main/SearchWithCategoryPage';
+import SearchWithCategory from './pages/Main/SearchWithCategoryPage';
+import ChatPage from './pages/Chat/ChatPage';
+import ChatRoomPage from './pages/Chat/ChatRoomPage';
 
 export default function App() {
   return (
@@ -17,15 +18,17 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/myInfo" element={<MyInfoPage />} />
-        <Route path="/chat" element={<ChattingPage />} />
         <Route path="/writing" element={<WritingPage />} />
         <Route path="/search" element={<SearchPage />}>
           <Route path=":searchValue" element={<SearchDetailPage />} />
         </Route>
-        {/* <Route path="/category" element={<SearchWithCategory />}>
+        <Route path="/category" element={<SearchWithCategory />}>
           <Route path=":name" element={<SearchWithCategory />} />
-        </Route> */}
+        </Route>
         <Route path="/detail/*" element={<SearchDetailPage />} />
+        <Route path="/chat" element={<ChatPage />}>
+          <Route path=":roomName" element={<ChatRoomPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
