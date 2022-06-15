41fcddeebf98fe640937541fac96dc84b411f795
ksy990628 <ksy990628@gmail.com>
2022-06-15 21:15:42 +0900

    feat : Add Icons to Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,16 +1,16 @@
 import colors from '@/constants/colors';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 import Participation from '../../assets/img/Participation.png';
 import PostI from '../../assets/img/post.png';
-import Notice from '../../assets/img/notice.png';
-import Setting from '../../assets/img/settings.png';
 import Shop from '../../assets/img/shop.png';
 import Location from '../../assets/img/Location.png';
 import Fee from '../../assets/img/fee_icon.png';
 import Time from '../../assets/img/time_icon.png';
 import Ppl from '../../assets/img/ppl_icon.png';
-import Bell from '../../assets/img/bell 1.png';
+import ModifyPost from '../../assets/img/ModifyPost.png';
+import DeletePost from '../../assets/img/DeletePost.png';
 import {useState} from 'react';
 
 interface PostInfo {
@@ -21,6 +21,34 @@ interface PostInfo {
     fee: string;
     [prop: string]: string;
   }
+
+const PostDiv = styled.div`
+display: flex;
+width: 90%;
+margin: 2rem auto;
+border: 1px solid #gray;
+border-radius: 8px;
+padding: 2rem;
+max-height: 30rem;
+box-shadow: 1px 1px 1px 1px ${colors.grey300};
+`;
+
+const LeftDiv = styled.div`
+//display: flex;
+flex-direction: column;
+width: 80%;
+`;
+
+const RightDiv = styled.div`
+display: flex;
+flex-direction: column;
+width: 20%;
+`;
+
+const IconWrapper = styled.div`
+width:30%;
+height:50%;
+`;
 
 export default function Post({title,location,peopleNum,time,fee}:PostInfo){
     const [postInfo, setPostInfo] = useState<PostInfo>({
@@ -33,24 +61,19 @@ export default function Post({title,location,peopleNum,time,fee}:PostInfo){
 
   return (
     <>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          width: 90%;
-          margin: 2rem auto;
-          border: 1px solid #gray;
-          border-radius: 8px;
-          padding: 2rem;
-          max-height: 30rem;
-          box-shadow: 1px 1px 1px 1px ${colors.grey300};
-        `}>
-            <h1>{title}</h1>
-            <img src = {Location}/> {location} 
-            {peopleNum}
-            {time}
-            {fee}
-      </div>
+    <PostDiv>
+        <LeftDiv>
+        <h1>{title}</h1>
+        <IconWrapper><img src = {Location} alt="배달 위치"/> {location}</IconWrapper> 
+        <IconWrapper><img src = {Ppl} alt="인원"/> {peopleNum}명</IconWrapper>
+        <IconWrapper><img src = {Time} alt="마감시간"/> {time}</IconWrapper>
+        <IconWrapper><img src = {Fee} alt="배달비"/> {fee}원</IconWrapper>
+        </LeftDiv>
+        <RightDiv>
+        <IconWrapper><img src = {DeletePost} alt="삭제하기"/></IconWrapper>
+        <IconWrapper><img src = {ModifyPost} alt="수정하기"/></IconWrapper>
+        </RightDiv>
+    </PostDiv>
     </>
   );
 }
