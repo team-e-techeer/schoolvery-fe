2967363194abe33d34023a398efd6f2eb03ad922
장동현 <54137044+ww8007@users.noreply.github.com>
2022-06-16 17:40:04 +0900

    fix: Remove checkBlank onBlur

--- src/pages/Writing/WritingPage.tsx
+++ src/pages/Writing/WritingPage.tsx
@@ -1,5 +1,20 @@
+/* eslint-disable import/extensions */
 import Header from '@/components/Header/Header';
-import { IconWrapper, Container, Input, InputBlock, NumInput, Post, TextArea, FirstSection, SecondSection, ThirdSection, IconBlock, SecondDetailSection, IconCenterBlock} from './WritingPage.styles';
+import {
+  IconWrapper,
+  Container,
+  Input,
+  InputBlock,
+  NumInput,
+  Post,
+  TextArea,
+  FirstSection,
+  SecondSection,
+  ThirdSection,
+  IconBlock,
+  SecondDetailSection,
+  IconCenterBlock,
+} from './WritingPage.styles';
 import { AiOutlineLeft as LeftIcon } from 'react-icons/ai';
 import { useState, useCallback, useRef } from 'react';
 import { useCheckBlank } from '@/hooks/useCheckBlank';
@@ -35,12 +50,12 @@ function WritingPage() {
   });
   const [submitOK, setSubmit] = useState(true);
 
-  const {title,store,category,location,peopleNum,closedTime,fee} = writingInfo;
+  const { title, store, category, location, peopleNum, closedTime, fee } = writingInfo;
 
   const onSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const submitItems = ['title','store','category','location','peopleNum','closedTime','fee'];
+      const submitItems = ['title', 'store', 'category', 'location', 'peopleNum', 'closedTime', 'fee'];
       submitItems.forEach(item => {
         if (!writingInfo[item].length) {
           setSubmit(false);
@@ -65,7 +80,7 @@ function WritingPage() {
 
   const onCheckBlank = useCallback(
     (e: React.FocusEvent<HTMLFormElement, Element>) => {
-      useCheckBlank({ e, ref: spanRef, state: writingInfo });
+      // useCheckBlank({ e, ref: spanRef, state: writingInfo });
     },
     [writingInfo, spanRef]
   );
@@ -73,57 +88,116 @@ function WritingPage() {
   return (
     <>
       <Header
-      title="글 등록"
+        title="글 등록"
         Left={() => (
           <IconWrapper>
             <Link to="/">
               <LeftIcon size={30} color="#fff" />
             </Link>
-          </IconWrapper> 
-          
+          </IconWrapper>
         )}
       />
       <Container ref={spanRef} onBlur={onCheckBlank} onSubmit={onSubmit}>
         <FirstSection>
           <InputBlock>
-            <IconBlock><img src={Shop} alt="제목"/></IconBlock>
-      <Input value={title} name="title" placeholder={'게시글 제목'} onChange={onChange} autoFocus={true} autoComplete="off"></Input>
-      </InputBlock>
-      <InputBlock>
-            <IconBlock><img src={Shop} alt="매장"/></IconBlock>
-      <Input value={store} name="store" placeholder={'주문할 매장을 입력해 주세요'} onChange={onChange} autoComplete="off"></Input>
-      </InputBlock>
-      <InputBlock>
-            <IconBlock><img src={Location} alt="카테고리"/></IconBlock>
-      <Input value={category} name="category" placeholder={'카테고리를 선택해 주세요'} onChange={onChange} autoComplete="off"></Input>
-      </InputBlock>
-      <InputBlock>
-            <IconBlock><img src={Location} alt="배달 위치"/></IconBlock>
-      <Input value={location} name="location" placeholder={'배달을 받을 위치를 입력해 주세요'} onChange={onChange} autoComplete="off"></Input>
-      </InputBlock>
-</FirstSection>
+            <IconBlock>
+              <img src={Shop} alt="제목" />
+            </IconBlock>
+            <Input
+              value={title}
+              name="title"
+              placeholder={'게시글 제목'}
+              onChange={onChange}
+              autoFocus={true}
+              autoComplete="off"
+            ></Input>
+          </InputBlock>
+          <InputBlock>
+            <IconBlock>
+              <img src={Shop} alt="매장" />
+            </IconBlock>
+            <Input
+              value={store}
+              name="store"
+              placeholder={'주문할 매장을 입력해 주세요'}
+              onChange={onChange}
+              autoComplete="off"
+            ></Input>
+          </InputBlock>
+          <InputBlock>
+            <IconBlock>
+              <img src={Location} alt="카테고리" />
+            </IconBlock>
+            <Input
+              value={category}
+              name="category"
+              placeholder={'카테고리를 선택해 주세요'}
+              onChange={onChange}
+              autoComplete="off"
+            ></Input>
+          </InputBlock>
+          <InputBlock>
+            <IconBlock>
+              <img src={Location} alt="배달 위치" />
+            </IconBlock>
+            <Input
+              value={location}
+              name="location"
+              placeholder={'배달을 받을 위치를 입력해 주세요'}
+              onChange={onChange}
+              autoComplete="off"
+            ></Input>
+          </InputBlock>
+        </FirstSection>
 
+        <SecondSection>
+          <SecondDetailSection>
+            <NumInput
+              placeholder={'인원수'}
+              name="peopleNum"
+              type={'number'}
+              pattern={'[0-9]*'}
+              value={peopleNum}
+              onChange={onChange}
+              autoComplete="off"
+            />
+            <IconCenterBlock>
+              <img src={Ppl} alt="인원" />
+            </IconCenterBlock>
+          </SecondDetailSection>
+          <SecondDetailSection>
+            <NumInput
+              placeholder={'마감 시간'}
+              name="closedTime"
+              value={closedTime}
+              onChange={onChange}
+              autoComplete="off"
+            />
+            <IconCenterBlock>
+              <img src={Time} alt="마감시간" />
+            </IconCenterBlock>
+          </SecondDetailSection>
+          <SecondDetailSection>
+            <NumInput
+              placeholder={'배달비'}
+              name="fee"
+              type={'number'}
+              pattern={'[0-9]*'}
+              value={fee}
+              onChange={onChange}
+              autoComplete="off"
+            />
+            <IconCenterBlock>
+              <img src={Fee} alt="배달비" />
+            </IconCenterBlock>
+          </SecondDetailSection>
+        </SecondSection>
 
-<SecondSection>
-  <SecondDetailSection>
-      <NumInput placeholder={'인원수'} name="peopleNum" type={'number'} pattern={"[0-9]*"} value={peopleNum} onChange={onChange} autoComplete="off"></NumInput>
-      <IconCenterBlock><img src={Ppl} alt="인원"/></IconCenterBlock>
-  </SecondDetailSection>
-  <SecondDetailSection>
-      <NumInput placeholder={'마감 시간'} name="closedTime" value={closedTime} onChange={onChange} autoComplete="off"></NumInput>
-      <IconCenterBlock><img src={Time} alt="마감시간"/></IconCenterBlock>
-  </SecondDetailSection>
-  <SecondDetailSection>
-      <NumInput placeholder={'배달비'} name="fee" type={"number"} pattern={"[0-9]*"} value={fee} onChange={onChange} autoComplete="off"></NumInput>
-      <IconCenterBlock><img src={Fee} alt="배달비"/></IconCenterBlock>
-  </SecondDetailSection>
-</SecondSection>
-      
-      <ThirdSection>
-      <TextArea name="detail"></TextArea>
-      </ThirdSection>
+        <ThirdSection>
+          <TextArea name="detail"></TextArea>
+        </ThirdSection>
 
-      <Post type="submit">글 등록</Post>
+        <Post type="submit">글 등록</Post>
       </Container>
     </>
   );
