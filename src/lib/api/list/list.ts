23d00df70f6f2d8b7e73d87b8425aaf135d249d2
장동현 <54137044+ww8007@users.noreply.github.com>
2022-07-27 21:38:51 +0900

    remove: Remove not used things

--- src/lib/api/list/list.ts
+++ src/lib/api/list/list.ts
@@ -1,8 +1,6 @@
 import client from '../client';
 import type { CategoryListAPI, CategoryListAPIResponse, PostListAPI, PostListWithCategoryAPI } from '@/interface/list';
 import { makeHeader } from '@/util/makeHeader';
-import { useRecoilValue } from 'recoil';
-import { userState } from '@/atoms/user/userState';
 
 // 포스트 목록 불러오기
 export const getPostListAPI = async ({ schoolId, accessToken }: PostListAPI): Promise<CategoryListAPIResponse> => {
