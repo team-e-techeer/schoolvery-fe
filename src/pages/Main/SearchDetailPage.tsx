34a0d86d61796a0e89f8e12535df60ed29980ed9
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-01 23:42:10 +0900

    feat: Add render category picture same with name

--- src/pages/Main/SearchDetailPage.tsx
+++ src/pages/Main/SearchDetailPage.tsx
@@ -5,8 +5,45 @@ import colors from '@/constants/colors';
 import { css } from '@emotion/react';
 import Logo from '../../assets/img/dog.png';
 import { useNavigate } from 'react-router-dom';
+import 중식 from '../../assets/img/ch.png';
+import 치킨 from '../../assets/img/Chicken.png';
+import 일식 from '../../assets/img/jp.png';
+import 한식 from '../../assets/img/ko.png';
+import 피자 from '../../assets/img/pizza.png';
+import All from '../../assets/img/all.png';
+import 아시안 from '../../assets/img/asian.png';
+import 햄버거 from '../../assets/img/hamburger.png';
+import 분식 from '../../assets/img/koreanFood.png';
+import 커피 from '../../assets/img/Coffee.png';
+import { useRecoilValue } from 'recoil';
+import { watchingPostState } from '@/atoms/user/watchingPostState';
+import { categoryListState } from '@/atoms/list/categoryListState';
+import { useEffect, useState } from 'react';
+
+interface ImageList {
+  [prop: string]: string;
+}
+
 export default function SearchDetailPage() {
   const navigate = useNavigate();
+  const watchingPost = useRecoilValue(watchingPostState);
+  const categoryList = useRecoilValue(categoryListState);
+  const [categoryName, setCategoryName] = useState('');
+  const [imageList] = useState<ImageList>({
+    중식: 중식,
+    치킨: 치킨,
+    일식: 일식,
+    한식: 한식,
+    피자: 피자,
+    아시안: 아시안,
+    햄버거: 햄버거,
+    분식: 분식,
+    커피: 커피,
+  });
+
+  useEffect(() => {
+    setCategoryName(categoryList.find(category => category.id === watchingPost.categoryId)?.name as string);
+  }, [categoryList, watchingPost]);
   return (
     <>
       <div
@@ -15,7 +52,7 @@ export default function SearchDetailPage() {
           height: 100vh;
         `}
       >
-        <HeaderWithGradient schoolName="00" imgSrc={Logo} categoryName="강아지" />
+        <HeaderWithGradient schoolName="00" imgSrc={imageList[categoryName]} categoryName={categoryName} />
         <JoinDetail
           shopName="BBQ 치킨"
           categoryName="치킨"
