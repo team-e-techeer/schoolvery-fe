78cee874e30e40ae28e52dac95374f449a8c5415
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-01 23:43:01 +0900

    feat: Add getMyPostListAPI

--- src/lib/api/list/list.ts
+++ src/lib/api/list/list.ts
@@ -2,6 +2,7 @@ import client from '../client';
 import type {
   CategoryListAPI,
   CategoryListAPIResponse,
+  MyPostListAPI,
   PostListAPI,
   PostListAPIResponse,
   PostListWithCategoryAPI,
@@ -15,9 +16,26 @@ export const getPostListAPI = async ({ schoolId, accessToken }: PostListAPI): Pr
   return data;
 };
 
-export const getPostListWithCategoryAPI = async ({ schoolId, categoryId, accessToken }: PostListWithCategoryAPI) => {
+export const getPostListWithCategoryAPI = async ({
+  schoolId,
+  categoryId,
+  accessToken,
+}: PostListWithCategoryAPI): Promise<PostListAPIResponse> => {
   const headers = makeHeader(accessToken);
-  const { data } = await client.get(`/api/v1/posts/school/${schoolId}/category/${categoryId}`, { headers });
+  const { data } = await client.get(`/api/v1/posts/school/${schoolId}/category/${categoryId}`, {
+    headers,
+    params: {
+      page: 1,
+      size: 15,
+    },
+  });
+  return data;
+};
+
+// 내 포스트 목록 불러오기
+export const getMyPostListAPI = async ({ userId, token }: MyPostListAPI): Promise<PostListAPIResponse> => {
+  const headers = makeHeader(token);
+  const { data } = await client.get(`/api/v1/posts/user/${userId}`, { headers });
   return data;
 };
 
