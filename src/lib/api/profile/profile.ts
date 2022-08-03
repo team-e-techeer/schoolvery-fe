6a39573fe3b7984881a0c9266d60ff42f7d33db4
ksy990628 <ksy990628@gmail.com>
2022-08-03 12:21:22 +0900

    feat: Add profile lib

---
+++ src/lib/api/profile/profile.ts
@@ -0,0 +1,17 @@
+import { UserAPI, UserAPIResponse, ProfileAPI, ProfileAPIResponse} from '@/interface/profile';
+import { makeHeader } from '@/util/makeHeader';
+import client from '../client';
+
+// 유저 정보 불러오기
+export const getUserAPI = async ({ userId, accessToken }: UserAPI): Promise<UserAPIResponse> => {
+    const headers = makeHeader(accessToken);
+    const { data } = await client.get(`/api/v1/users/${userId}`, { headers });
+    return data;
+  };
+
+// 유저 정보 수정하기
+export const API = async (profileInfo: ProfileAPI): Promise<ProfileAPIResponse> => {
+    const headers = { 'Content-type': 'Application/json', Accept: '*/*' };
+    const { data } = await client.patch(`/api/v1/users/${profileInfo.userId}`, JSON.stringify(profileInfo), { headers });
+    return data;
+  };
