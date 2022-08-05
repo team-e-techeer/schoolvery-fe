f9bf4ebcc1e8adbace89b77d4e86fbd0feec31f9
ksy990628 <ksy990628@gmail.com>
2022-08-05 21:04:29 +0900

    chore: Modify usePatchUserQuery

--- src/hooks/query/profile/usePatchUser.ts
+++ src/hooks/query/profile/usePatchUser.ts
@@ -2,6 +2,6 @@ import type { ProfileAPI } from '@/interface/profile';
 import { patchUserAPI } from '@/lib/api/profile/profile';
 import { useMutation } from 'react-query';
 
-export const usePatchUserQuery = (request: ProfileAPI) => {
-    return useMutation(['User', request.accessToken, request.userId], async () => patchUserAPI(request));
+export const usePatchUserQuery = () => {
+    return useMutation((UserInfo: ProfileAPI) => patchUserAPI(UserInfo));
   };
