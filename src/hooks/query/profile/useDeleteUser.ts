3aea1b98bed2e913a3a365b7c97e428c371ed838
ksy990628 <ksy990628@gmail.com>
2022-08-10 11:06:00 +0900

    chore: Update react query

--- src/hooks/query/profile/useDeleteUser.ts
+++ src/hooks/query/profile/useDeleteUser.ts
@@ -2,6 +2,6 @@ import type { UserAPI } from '@/interface/profile';
 import { deleteUserAPI } from '@/lib/api/profile/profile';
 import { useMutation } from 'react-query';
 
-export const usePatchUserQuery = () => {
+export const useDeleteUserMutation = () => {
     return useMutation((UserInfo: UserAPI) => deleteUserAPI(UserInfo));
   };
--- src/hooks/query/profile/usePatchUser.ts
+++ src/hooks/query/profile/usePatchUser.ts
@@ -2,6 +2,6 @@ import type { ProfileAPI } from '@/interface/profile';
 import { patchUserAPI } from '@/lib/api/profile/profile';
 import { useMutation } from 'react-query';
 
-export const usePatchUserQuery = () => {
+export const usePatchUserMutation = () => {
     return useMutation((UserInfo: ProfileAPI) => patchUserAPI(UserInfo));
   };
