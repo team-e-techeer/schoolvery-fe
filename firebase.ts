4ff8b19fc90f13cc6846853a78646a380d550216
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-11 23:09:01 +0900

    feat: Add firebaseConfig setting

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: import.meta.env['VITE_API_KEY'],
+  authDomain: import.meta.env['VITE_AUTH_DOMAIN'],
+  projectId: import.meta.env['VITE_PROJECT_ID'],
+  storageBucket: import.meta.env['VITE_BUCKET'],
+  messagingSenderId: import.meta.env['VITE_MESSAGE_SENDER_ID'],
+  appId: import.meta.env['VITE_APP_ID'],
+  measurementId: import.meta.env['VITE_MEASUREMENTID'],
+  databaseURL: import.meta.env['VITE_DB_URL'],
+};
+
+console.log('yrk', import.meta.env['DB_URL']);
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+
+export default app;
