4b260306ae77274fae40015dc3a1c0df892dd686
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-13 20:51:38 +0900

    feat: Add getSchoolListAPI return type

--- src/lib/api/auth/school.ts
+++ src/lib/api/auth/school.ts
@@ -1,6 +1,7 @@
+import { SchoolListResponse } from '@/interface/list';
 import client from '../client';
 
-export const getSchoolListAPI = async () => {
+export const getSchoolListAPI = async (): Promise<SchoolListResponse> => {
   const headers = { 'Content-type': 'Application/json', Accept: '*/*' };
   const { data } = await client.get('/api/v1/school', { headers });
   return data;
