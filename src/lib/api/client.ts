54a87ae11281145e9b14b07d4f3e97019d10c00a
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-14 01:52:13 +0900

    fix: Fix clientBaseURL from .env

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const client = axios.create();
 
-// export const clientBaseURL = import.meta.env['VITE_API_URL'];
+export const clientBaseURL = import.meta.env['VITE_API_URL'];
 
-client.defaults.baseURL = 'http://localhost:8080';
+client.defaults.baseURL = clientBaseURL;
 client.defaults.headers.options = {
   'Content-type': 'Application/json',
   Accept: '*/*',
