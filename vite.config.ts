0d686bdba8603aebcb66a95ea8d32d640908527f
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-15 13:47:14 +0900

    chore: Add split code

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { splitVendorChunkPlugin } from 'vite';
+import fs from 'fs';
+import path from 'path';
+// eslint-disable-next-line import/extensions
+import { dependencies } from './package.json';
+import dynamicImport from 'vite-plugin-dynamic-import';
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach(key => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -15,6 +28,29 @@ export default defineConfig({
       },
     }),
     splitVendorChunkPlugin(),
+    dynamicImport({
+      onFiles: files => files.filter(f => !f.includes('main-output.js')),
+    }),
+    {
+      name: 'vite-plugin-dynamic-import:test',
+      transform(code, id) {
+        if (/src\/main\.ts$/.test(id)) {
+          // Write transformed code to main-output.js
+          fs.writeFileSync(path.join(path.dirname(id), 'main-output.js'), code);
+        }
+      },
+    },
   ],
-  base: '/schoolvery-fe/',
+  base: './',
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
